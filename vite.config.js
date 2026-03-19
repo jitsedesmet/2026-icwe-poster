fb6ff7dd733dc5d65f74d63a581bf2f5d0a34d29
Jitse De Smet <jitse@reedonk8.be>
2026-03-19 10:40:04 +0100

    cleanup pdf generation

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig(() => ({
-    // Keep relative URLs for both hosted and file:// output.
+    plugins: [ viteSingleFile() ],
     base: './',
 }))
